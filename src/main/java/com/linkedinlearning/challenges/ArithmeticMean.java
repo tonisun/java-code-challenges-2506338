8af09c865860eb6350bf538b47766a37df2f2b20
Toni Zeidler <toni.z@gmx.de>
2023-09-01 19:44:50 +0000

    add ArithmeticMean & ArithmeticMeanTest

--- src/main/java/com/linkedinlearning/challenges/ArithmeticMean.java
+++ src/main/java/com/linkedinlearning/challenges/ArithmeticMean.java
@@ -3,7 +3,6 @@ package com.linkedinlearning.challenges;
 import java.util.Arrays;
 import java.util.Collection;
 
-
 public class ArithmeticMean {
 
   public static double mean(Collection<Integer> numbers) {
@@ -13,15 +12,28 @@ public class ArithmeticMean {
     int sum = 0;
     int count = 0;
     for (int number : numbers) {
-        sum += number;
-        count++;
+      sum += number;
+      count++;
     }
     return (double) sum / count;
+  }
+
+  public static double mean2(Collection<Integer> numbers) {
+    if (numbers == null || numbers.isEmpty()) {
+      throw new IllegalArgumentException("Collection cannot be null or empty");
+    }
+    return numbers.stream()
+        .mapToInt(Integer::intValue)
+        .average()
+        .orElse(.0);
   }
 
   public static void main(String[] args) {
     double mean = ArithmeticMean.mean(Arrays.asList(1, 2, 3, 4, 5));
     System.out.println("The arithmetic mean is: " + mean);
+
+    double mean2 = ArithmeticMean.mean2(Arrays.asList(1, 2, 3, 4, 5));
+    System.out.println("The arithmetic mean is: " + mean2);
   }
 
 }
--- src/test/java/com/linkedinlearning/challenges/ArithmeticMeanTest.java
+++ src/test/java/com/linkedinlearning/challenges/ArithmeticMeanTest.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
 public class ArithmeticMeanTest {
+
   @Test
   public void testMean() {
     double actual = ArithmeticMean.mean(Arrays.asList(1, 2, 3, 4, 5));
@@ -24,5 +25,22 @@ public class ArithmeticMeanTest {
   @Test
   public void testMeanNullCollection() {
     assertThrows(IllegalArgumentException.class, () -> ArithmeticMean.mean(null));
+  }
+
+  @Test
+  public void testMean2() {
+    double actual = ArithmeticMean.mean2(Arrays.asList(1, 2, 3, 4, 5));
+    double expected = 3.0;
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testMean2EmptyCollection() {
+    assertThrows(IllegalArgumentException.class, () -> ArithmeticMean.mean2(Collections.emptyList()));
+  }
+
+  @Test
+  public void testMean2NullCollection() {
+    assertThrows(IllegalArgumentException.class, () -> ArithmeticMean.mean2(null));
   }
 }
